1b6359a24316e1819c6e3fefc110bc90915b8945
pqnarte <37677471+pqnarte@users.noreply.github.com>
2018-09-13 17:21:56 +0100

    Delete map.py

--- map.py
+++
@@ -1,9 +0,0 @@
-class Map(list):
-
-    def __init__(self,mapName,maplayoutURL,mapvideo):
-        self.mapName = mapName
-        self.maplayoutURL = maplayoutURL
-        self.mapvideo = mapvideo
-
-    def __repr__(self):
-        return "Weapon('{}', '{}', '{}', '{}')".format(self.weaponName,self.description,self.weaponClass,self.imageURL)
